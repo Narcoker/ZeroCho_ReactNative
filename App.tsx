44daf7f49f3c07651a50d1640e762e01cad8e99b
Narcoker <jykim9868@gmail.com>
2022-05-03 23:15:04 +0900

    Chapter 2-2

--- App.tsx
+++ App.tsx
@@ -1,65 +1,68 @@
 import * as React from 'react';
-import { NavigationContainer, ParamListBase } from '@react-navigation/native';
-import {
-  createNativeStackNavigator,
-  NativeStackScreenProps,
-} from '@react-navigation/native-stack';
-import { Pressable, Button, TouchableWithoutFeedback, TouchableNativeFeedback, TouchableHighlight, Text, View } from 'react-native';
-import { useCallback } from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Settings from './src/pages/Settings';
+import Orders from './src/pages/Orders';
+import Delivery from './src/pages/Delivery';
+import {useState} from 'react';
+import SignIn from './src/pages/SignIn';
+import SignUp from './src/pages/SignUp';
 
-type RootStackParamList = {
-  Home: undefined;
-  Details: undefined;
+export type LoggedInParamList = { // 로그인했을때 출력되는 화면
+  Orders: undefined; // 주문 화면
+  Settings: undefined; // 
+  Delivery: undefined; // 주문 수락시 고객집과 가게의 경로 출력
+  Complete: { orderId: string }; // 배달 완료 처리 화면: 고객에게 배달완료 된 사진을 보여줌, orderId 는 주문정보를 받아오는 파라미터이다.
+  // 파라미터는 다른페이지의 정보를 가지고 오고싶을 때 사용하는 것이 좋다.
 };
-type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
-type DetailsScreenProps = NativeStackScreenProps<ParamListBase, 'Details'>;
 
-function HomeScreen({ navigation }: HomeScreenProps) {
-  const onClick = useCallback(() => {
-    navigation.navigate('Details');
-  }, [navigation]);
+export type RootStackParamList = { //로그인 안했을때 출력되는 화면
+  SignIn: undefined; //로그인 화면
+  SignUp: undefined; //회원가입 화면
+};
 
-  return (
-    < >
-      <View style={{ flex: 2, backgroundColor: 'yellow', alignItems: 'flex-end', justifyContent: 'center' }}>
-        <Pressable onPress={onClick} style={{
-          paddingHorizontal: 40, paddingVertical: 20, backgroundColor: "blue"
-        }}>
-          <Text style={{ color: 'white' }}>Home Screen</Text>
-        </Pressable>
-      </View>
-      <View style={{ flex: 5, backgroundColor: 'orange' }}><Text>Second</Text></View>
-    </>
-  );
-}
+// type을 나눠두면 좋은 이유: 혹시나 로그인화면에서 setting을 넘어가는 말도안되는 현상을 막기 위한 대비책
 
-function DetailsScreen({ navigation }: DetailsScreenProps) {
-  const onClick = useCallback(() => {
-    navigation.navigate('Home');
-  }, [navigation]);
-
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Pressable onPress={onClick}>
-        <Text>Details Screen</Text>
-      </Pressable>
-    </View>
-  );
-}
-
+const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
+
 function App() {
+  const [isLoggedIn, setLoggedIn] = useState(true);
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: '홈화면' }}
-        />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-
-      </Stack.Navigator>
+      {isLoggedIn ? (
+        <Tab.Navigator>
+          <Tab.Screen
+            name="Orders"
+            component={Orders}
+            options={{title: '오더 목록'}}
+          />
+          <Tab.Screen
+            name="Delivery"
+            component={Delivery}
+            options={{headerShown: false}}
+          />
+          <Tab.Screen
+            name="Settings"
+            component={Settings}
+            options={{title: '내 정보'}}
+          />
+        </Tab.Navigator>
+      ) : (
+        <Stack.Navigator>
+          <Stack.Screen
+            name="SignIn"
+            component={SignIn}
+            options={{title: '로그인'}}
+          />
+          <Stack.Screen
+            name="SignUp"
+            component={SignUp}
+            options={{title: '회원가입'}}
+          />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 }
